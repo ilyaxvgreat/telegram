bf69a060b78ba1a81dbb9dc7806816eeed8dd14c
illia-khomchenko <illia.khomchenko@ardas.biz>
2024-12-12 18:47:17 +0000

    first commit

---
+++ src/test/java/com/khomchenko/telegram/TelegramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khomchenko.telegram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
